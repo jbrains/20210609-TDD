3a80a20bcef000f44dafbd577546eee8dcf52afd
J. B. Rainsberger <me@jbrains.ca>
2021-06-09 11:35:15 -0300

    [FAILING TEST] Drafted a test for adding a non-zero integer to zero.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -10,6 +10,12 @@ public class AddFractionsTest {
         Assertions.assertEquals(0, sum.intValue());
     }
 
+    @Test
+    void notZeroPlusZero() throws Exception {
+        final Fraction sum = new Fraction(4).plus(new Fraction(0));
+        Assertions.assertEquals(4, sum.intValue());
+    }
+
     public static class Fraction {
         public Fraction(final int integerValue) {
         }
