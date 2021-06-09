7b0398f41dd5820e976ae974a1ff69685963cc1f
J. B. Rainsberger <me@jbrains.ca>
2021-06-09 11:46:34 -0300

    Put related tests in a grouping, because the upcoming tests are different.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -4,30 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AddFractionsTest {
-    @Test
-    void zeroPlusZero() throws Exception {
-        final Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assertions.assertEquals(0, sum.intValue());
-    }
+    public static class BackwardsCompatibleWithIntegers {
+        @Test
+        void zeroPlusZero() throws Exception {
+            final Fraction sum = new Fraction(0).plus(new Fraction(0));
+            Assertions.assertEquals(0, sum.intValue());
+        }
 
-    @Test
-    void notZeroPlusZero() throws Exception {
-        final Fraction sum = new Fraction(4).plus(new Fraction(0));
-        Assertions.assertEquals(4, sum.intValue());
-    }
+        @Test
+        void notZeroPlusZero() throws Exception {
+            final Fraction sum = new Fraction(4).plus(new Fraction(0));
+            Assertions.assertEquals(4, sum.intValue());
+        }
 
-    @Test
-    void zeroPlusNotZero() throws Exception {
-        final Fraction sum = new Fraction(0).plus(new Fraction(6));
-        Assertions.assertEquals(6, sum.intValue());
-    }
+        @Test
+        void zeroPlusNotZero() throws Exception {
+            final Fraction sum = new Fraction(0).plus(new Fraction(6));
+            Assertions.assertEquals(6, sum.intValue());
+        }
 
-    @Test
-    void nonZeroIntegers() throws Exception {
-        final Fraction sum = new Fraction(3).plus(new Fraction(5));
-        Assertions.assertEquals(8, sum.intValue());
+        @Test
+        void nonZeroIntegers() throws Exception {
+            final Fraction sum = new Fraction(3).plus(new Fraction(5));
+            Assertions.assertEquals(8, sum.intValue());
+        }
     }
-
+    
     public static class Fraction {
         private final int integerValue;
 
