9eabee833150a3f3f15ca2d2661051d132ff06aa
J. B. Rainsberger <me@jbrains.ca>
2021-06-09 11:32:35 -0300

    [FAILING TEST] Generated enough code to make the test run (and fail).

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -8,4 +8,13 @@ public class AddFractionsTest {
     void zeroPlusZero() throws Exception {
         Assertions.assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
+
+    public static class Fraction {
+        public Fraction(final int integerValue) {
+        }
+
+        public Fraction plus(final Fraction that) {
+            return new Fraction(0);
+        }
+    }
 }
