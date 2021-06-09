b94be60532d49a768cca1dcafbd822f0513311c3
J. B. Rainsberger <me@jbrains.ca>
2021-06-09 12:25:19 -0300

    All tests now check Fractions for equals() instead of checking the individual parts.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -15,45 +15,39 @@ public class AddFractionsTest {
         @Test
         void notZeroPlusZero() throws Exception {
             final Fraction sum = new Fraction(4).plus(new Fraction(0));
-            Assertions.assertEquals(4, sum.intValue());
+            Assertions.assertEquals(new Fraction(4), sum);
         }
 
         @Test
         void zeroPlusNotZero() throws Exception {
             final Fraction sum = new Fraction(0).plus(new Fraction(6));
-            Assertions.assertEquals(6, sum.intValue());
+            Assertions.assertEquals(new Fraction(6), sum);
         }
 
         @Test
         void nonZeroIntegers() throws Exception {
             final Fraction sum = new Fraction(3).plus(new Fraction(5));
-            Assertions.assertEquals(8, sum.intValue());
+            Assertions.assertEquals(new Fraction(8), sum);
         }
     }
 
     @Test
     void sameDenominators() throws Exception {
         final Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
-
-        Assertions.assertEquals(3, sum.getNumerator());
-        Assertions.assertEquals(5, sum.getDenominator());
+        Assertions.assertEquals(new Fraction(3, 5), sum);
     }
 
     @Test
     void relativelyPrimeDenominators() throws Exception {
         final Fraction sum = new Fraction(3, 7).plus(new Fraction(2, 5));
-
-        Assertions.assertEquals(29, sum.getNumerator());
-        Assertions.assertEquals(35, sum.getDenominator());
+        Assertions.assertEquals(new Fraction(29, 35), sum);
     }
 
     @Test
     @Disabled("refactoring")
     void denominatorsWithACommonFactor() throws Exception {
         final Fraction sum = new Fraction(3, 8).plus(new Fraction(5, 12));
-
-        Assertions.assertEquals(19, sum.getNumerator());
-        Assertions.assertEquals(24, sum.getDenominator());
+        Assertions.assertEquals(new Fraction(19, 24), sum);
     }
 
 
