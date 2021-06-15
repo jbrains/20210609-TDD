7912abc872be56b2d574fadd19f40130c8cd555d
J. B. Rainsberger <me@jbrains.ca>
2021-06-15 10:55:24 -0300

    Documented what happens to our command interpreter when there are trailing line separators.

--- app/src/test/java/ca/jbrains/pos/test/InterpretCommandsFromConsoleTest.java
+++ app/src/test/java/ca/jbrains/pos/test/InterpretCommandsFromConsoleTest.java
@@ -37,6 +37,29 @@ public class InterpretCommandsFromConsoleTest {
                 .stream().forEachOrdered(checkCommandOrder.verify(controller)::onCommand);
     }
 
+    @Test
+    void severalCommands_IgnoringTheTrailingLineSeparator() throws Exception {
+        interpretCommands(controller, new StringReader("::command 1::\n::command 2::\n::command 3::\n"));
+
+        final InOrder checkCommandOrder = Mockito.inOrder(controller);
+        Arrays.asList("::command 1::", "::command 2::", "::command 3::")
+                .stream().forEachOrdered(checkCommandOrder.verify(controller)::onCommand);
+    }
+
+    @Test
+    void oneCommand_IgnoringTheTrailingLineSeparator() throws Exception {
+        interpretCommands(controller, new StringReader("12345\n"));
+
+        Mockito.verify(controller).onCommand("12345");
+    }
+
+    @Test
+    void onlyALineSeparator() throws Exception {
+        interpretCommands(controller, new StringReader("\n"));
+
+        Mockito.verify(controller).onCommand("");
+    }
+
     private void interpretCommands(final Controller controller, final StringReader commandSource) throws IOException {
         new BufferedReader(commandSource).lines().forEachOrdered(controller::onCommand);
     }
