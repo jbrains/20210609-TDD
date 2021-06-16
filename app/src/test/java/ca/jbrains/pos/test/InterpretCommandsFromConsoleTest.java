46b86d2edd75342f768aa56cc315740159634c26
J. B. Rainsberger <me@jbrains.ca>
2021-06-16 08:14:24 -0300

    Improved formatting.

--- app/src/test/java/ca/jbrains/pos/test/InterpretCommandsFromConsoleTest.java
+++ app/src/test/java/ca/jbrains/pos/test/InterpretCommandsFromConsoleTest.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Arrays;
-import java.util.List;
 
 public class InterpretCommandsFromConsoleTest {
 
